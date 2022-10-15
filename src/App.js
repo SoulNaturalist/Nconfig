3d41788e99dd470f6c4f16aac35ec8a0edd4f7ea
ResponseGood <omg12121@mail.ru>
2022-10-15 21:35:06 +0300

    wrapped application by browser router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Header from "./components/Header";
 import WelcomePage from "./components/WelcomePage";
 import DeleteConfig from "./components/DeleteConfigPage"
 import CreateConfig from "./components/CreateConfigPage"
-import {HashRouter,Route} from "react-router-dom";
+import {Route,Routes} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header/>
-      <HashRouter>
-        <Route path="/Nconfig/" element={<WelcomePage />} />
-        <Route path="/Nconfig/delete" element={<DeleteConfig />} />
-        <Route path="/Nconfig/create" element={<CreateConfig />} />
-      </HashRouter>
+          <Routes>
+            <Route path="/Nconfig/" element={<WelcomePage />} />
+            <Route path="/Nconfig/delete" element={<DeleteConfig />} />
+            <Route path="/Nconfig/create" element={<CreateConfig />} />
+          </Routes>
     </div>
   );
 }
