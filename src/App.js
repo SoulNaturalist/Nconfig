0e423256433511b019d25d72a478ad79f2b06708
ResponseGood <omg12121@mail.ru>
2022-10-15 20:37:01 +0300

    try fixed routing

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import Header from "./components/Header";
 import WelcomePage from "./components/WelcomePage";
 import DeleteConfig from "./components/DeleteConfigPage"
 import CreateConfig from "./components/CreateConfigPage"
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import {HashRouter,Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header/>
-      <Router>
-        <Fragment>
-          <Routes>
-            <Route path="/Nconfig/" element={<WelcomePage />} />
-            <Route path="/Nconfig/delete" element={<DeleteConfig />} />
-            <Route path="/Nconfig/create" element={<CreateConfig />} />
-          </Routes>
-        </Fragment>
-      </Router>
+      <HashRouter basename="/">
+        <Route path="/Nconfig/" element={<WelcomePage />} />
+        <Route path="/Nconfig/delete" element={<DeleteConfig />} />
+        <Route path="/Nconfig/create" element={<CreateConfig />} />
+      </HashRouter>
     </div>
   );
 }
